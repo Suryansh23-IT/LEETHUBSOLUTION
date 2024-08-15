db10dc02436aeb8d73a733e469a2c3704928ddd0
Suryansh23-IT <suryanshdwivedi567@gmail.com>
2024-08-16 00:53:05 +0530

    Time: 0 ms (100%), Space: 8.6 MB (27.12%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,11 +2,16 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         if(x<0) return 0;
-        string str=to_string(x);
-        int l=str.length();
-        for(int i=0;i<l/2;i++){
-            if(str[i]!=str[l-i-1]) return 0;
+        int org=x,rev=0;
+        while(x>0){
+            if(rev>INT_MAX/10) return 0;
+            int digit=x%10;
+            rev=rev*10+digit;
+            x=x/10;
         }
-        return 1;
+        if(org==rev){
+            return 1;
+        }
+        return 0;
     }
 };
